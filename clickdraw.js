c526d1b4ac57a0cd91a1fb9f8d87e8544cfee6a8
Caleb Smith-Salzberg <csmithsalzberg@stuy.edu>
2018-02-08 22:44:12 -0500

    Update clickdraw.js

--- clickdraw.js
+++ clickdraw.js
@@ -1,7 +1,7 @@
 //Caleb Smith-Salzberg
 //SoftDev2 pd7
-//k2
-//2018-01-07
+//k3
+//2018-01-08
 
 var c = document.getElementById("slate");
 //slate is the canvas
@@ -13,11 +13,14 @@ var size;
 var increment=1;
 var x;
 var y;
+var dx;
+var dy;
 
 var stop = document.getElementById("s");
 var mode = document.getElementById("m");
+var dvd = document.getElementById("screensaver");
 
-var draw = function(){
+/*var draw = function(){
     ctx.beginPath();
     ctx.clearRect(0, 0, c.width, c.height); //clears screen
     ctx.fillStyle = "red";
@@ -32,16 +35,10 @@ var draw = function(){
 	id = window.requestAnimationFrame(draw);
     }
     else{
-	stopball();
+	changeMode();
+	id = window.requestAnimationFrame(draw);
     }
-}
-var onClick = function(e){
-    x= e.offsetX;
-    y= e.offsetY;
-    size = 10;
-    window.requestAnimationFrame(draw);
-}
-c.addEventListener("click", onClick);
+}*/
 
 var changeMode = function(){
     increment *= -1;
@@ -55,3 +52,47 @@ var stopball = function(){
   }
 }
 stop.addEventListener("click", stopball);
+
+var onClick = function(e){
+    dx = Math.floor(Math.random()*5 + 1)
+    dy = Math.floor(Math.random()*5 + 1)
+    ctx.clearRect(0, 0, c.width, c.height); //clears screen
+    x= e.offsetX;
+    y= e.offsetY;
+    size = 50;
+    window.requestAnimationFrame(bounce);
+}
+c.addEventListener("click", onClick);
+
+
+var startBounce = function(){
+    dx = Math.floor(Math.random()*5 + 1)
+    dy = Math.floor(Math.random()*5 + 1)
+    ctx.clearRect(0, 0, c.width, c.height); //clears screen
+    size = 50;
+    x=c.width/2;
+    y=c.height/2;
+    window.requestAnimationFrame(bounce);
+}
+dvd.addEventListener("click", startBounce);
+
+
+var bounce = function(){
+    ctx.clearRect(0, 0, c.width, c.height); //clears screen
+    ctx.beginPath();
+    ctx.fillStyle = "red";
+    ctx.strokeStyle="red";
+    ctx.arc(x, y, size, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.fill();
+    if(x+dx+size > 600 || x+dx-size < 0){
+	dx*=-1;
+    }
+    x+=dx
+    if(y+dy+size > 600 || y+dy-size < 0){
+	dy*=-1;
+    }
+    y+=dy
+
+    window.requestAnimationFrame(bounce);
+}
